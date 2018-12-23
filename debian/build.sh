9dcbf054b660695b1ff329514eba8d24179b1c78
Christopher Hoskin <christopher.hoskin@gmail.com>
2018-12-23 22:26:37 +0000

    Build with docker

---
+++ debian/build.sh
@@ -0,0 +1,6 @@
+cd /src/f-spot
+# Danger, the following command will zap any uncommited changes to /debian
+# git clean -x -f -d
+git submodule foreach git clean -x -f -d
+nuget restore
+gbp buildpackage --git-debian-branch=ubuntu/bionic --git-upstream-tree=master --git-submodules --git-export-dir=~/build/ --git-verbose
