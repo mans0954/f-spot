cd2732d4fe8184454e9ec01d74b53be40758287a
Christopher Hoskin <christopher.hoskin@gmail.com>
2018-12-26 09:39:06 +0000

    Remove old orig tar ball to force regen

--- debian/build.sh
+++ debian/build.sh
@@ -1,6 +1,8 @@
 cd /src/f-spot
 # Danger, the following command will zap any uncommited changes to /debian
 # git clean -x -f -d
+UPSTREAM_VERSION=`dpkg-parsechangelog --show-field Version | sed 's/-[^-]*$//'`
+rm ../f-spot_$UPSTREAM_VERSION.orig.tar.gz
 git submodule foreach git clean -x -f -d
 git submodule update
 nuget restore
